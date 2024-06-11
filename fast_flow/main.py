4461900a407ca8b93dc5c09b98a5f73c1f594b3b
kiran <kirannadimpalli2000@gmail.com>
2024-06-11 16:35:33 +0530

    fix: added some more dependency

---
+++ fast_flow/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from celery import Celery
+
+celery = Celery(
+    __name__,
+    broker="redis://127.0.0.1:6379/0",
+    backend="redis://127.0.0.1:6379/0"
+)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@celery.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x / y
